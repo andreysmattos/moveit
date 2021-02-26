08289dcce247870ac15b9cc4dd3979783919ed5e
Andrey Mattos <andreysmattos@hotmail.com>
2021-02-25 22:51:45 -0300

    feat

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,8 @@ import Head from 'next/head';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
+import { Countdown } from '../components/countDown';
 import Styles from '../styles/pages/Home.module.css'
-
-
-
-// import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -17,13 +14,18 @@ export default function Home() {
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
       </Head>
 
+      <Head>
+        <title> Inicio | move.it </title>
+      </Head>
+
       <div className={Styles.container}>
         <ExperienceBar />
 
         <section>
           <div>
-              <Profile/>
-              <CompletedChallenges />
+            <Profile />
+            <CompletedChallenges />
+            <Countdown></Countdown>
           </div>
 
           <div></div>
